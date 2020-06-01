2d56428d079acbde25ccefc18b26e4641cf26031
MinimumDelta <MinimumDelta@GMail.com>
2020-05-31 22:05:04 -0500

    normalized some language to be more clear and concise added more config options related to profile data harvesting (not images) general cleanup

--- src/bot_config.py
+++ src/bot_config.py
@@ -1,28 +1,45 @@
 LOG_PROFILE_DATA = True
 
+# data is stored in the below order with index 0 being the profile uid
+LOG_PROFILE_NAME = True
+LOG_PROFILE_AGE = True
+LOG_PROFILE_DISTANCE = True
+LOG_PROFILE_SWIPE = True  # this will be false if AUTO_SWIPE is false
+LOG_PROFILE_BIO = True
+LOG_PROFILE_IMAGE_URLS = True
+LOG_PROFILE_INSTAGRAM_URLS = True
+
+PROFILE_DATA_SEPARATOR = ';'  # this character will be removed from all bios
+
+# if true the bot will swipe according to the below criteria
+AUTO_SWIPE = True
+
 # swipe settings
 SWIPE_MIN_AGE = 18
 SWIPE_MAX_AGE = 25
 # set to -1 to include users that have a hidden location
 SWIPE_MIN_DISTANCE = 0
 SWIPE_MAX_DISTANCE = 25
-# minimum profile pictures needed in order to swipe right
+# minimum profile pictures (not instagram) needed in order to swipe right
 SWIPE_MIN_IMAGES = 3
 
 # highest age to download images 
-IMAGE_AGE_MAX = 28
+DOWNLOAD_IMAGE_AGE_MAX = 28
 # if instagram images should be downloaded
-IMAGE_INSTAGRAM = True
+DOWNLOAD_IMAGE_INSTAGRAM = True
 # if profile images should be downloaded
-IMAGE_PROFILE = True
+DOWNLOAD_IMAGE_PROFILE = True
 
 # below settings are not meant to be configured
-DELAY_INTERVAL_SECONDS = 0.25
+API_REQUEST_DELAY_SECONDS = 0.5  # forced throttle between API requests
 WORKER_TIMEOUT_SECONDS = 30
-POOL_WORKERS = 32
+EMPTY_RESPONSE_ATTEMPTS = 3  # if no persons returned after this many attempts, the bot will terminate
+POOL_WORKERS = 39  # one worker for each image
+MAX_POSSIBLE_PROFILE_IMAGES = 39
 
+# WARNING if file name is changed, git will not ignore it and can publish your token (not good)
 TOKEN_FILE_NAME = "X-AUTH-KEY"
-MIN_TOKEN_LENGTH = 30
+MIN_TOKEN_LENGTH = 30  # TODO determine if token length is static else the correct min value
 
 CODE_REQUEST_URL = "https://api.gotinder.com/v2/auth/sms/send?auth_type=sms"
 CODE_VALIDATE_URL = "https://api.gotinder.com/v2/auth/sms/validate?auth_type=sms"
@@ -31,6 +48,10 @@ TOKEN_URL = "https://api.gotinder.com/v2/auth/login/sms"
 GET_USERS_URL = "https://api.gotinder.com/user/recs"
 LIKE_USER_URL = "https://api.gotinder.com/like"  # add /userid to the end
 DISLIKE_USER_URL = "https://api.gotinder.com/pass"  # add /userid to the end
+
+IMAGE_OUTPUT_PATH = "output/image_output"
+DATA_OUTPUT_PATH = "output"
+DATA_OUTPUT_ENCODING = "utf-16"  # 16 required to handle emojis and non-alphabetic languages
 
 HEADERS = {  # headers used for api calls -> must append auth token after retrieval
     'user-agent': 'Tinder/11.4.0 (iPhone; iOS 12.4.1; Scale/2.00)',
--- src/tinder_bot.py
+++ src/tinder_bot.py
@@ -10,7 +10,7 @@ import shutil  # image downloader
 import bot_config
 
 
-class Person:
+class Profile:
     def __init__(self, name, age, distance, bio, profile_images, instagram_images):
         self.name = name
         self.age = age
@@ -35,7 +35,7 @@ def get_refresh_token(otp_code, phone_number):
     req = requests.post(bot_config.CODE_VALIDATE_URL, headers=bot_config.HEADERS, data=json.dumps(data))
     response = req.json()
     try:
-        return response.get("data")["refresh_token"]  # if response.get("data")["validated"] else False
+        return response.get("data")["refresh_token"]
     except Exception:
         return ""
 
@@ -70,68 +70,70 @@ def generate_new_token():
 
     ret_token = get_api_token(ret_refresh_token)
 
-    print("[INFO] Your new Tinder token is: {}".format(ret_token))
+    print(f"[INFO] Your new Tinder token is: {ret_token}")
     try:
         with open(bot_config.TOKEN_FILE_NAME, 'w') as ostream:
-            ostream.write(
-                "{}\nDELETE THIS FILE IN ORDER TO FORCE NEW KEY RETRIEVAL\nDO NOT MODIFY THIS FILE".format(ret_token))
+            ostream.write(f"{ret_token}\n"
+                          f"DELETE THIS FILE IN ORDER TO FORCE NEW KEY RETRIEVAL\n"
+                          f"DO NOT MODIFY THIS FILE")
         print("[INFO] Your token has been stored for future use.")
     except Exception as e:
-        print("[ERROR] Unable to save token. |=> {}".format(e))
+        print(f"[ERROR] Unable to save token. |=> {e}")
     return ret_token
 
 
-def valid_request(request_obj):
+def is_valid_response_status(request_obj):
     return request_obj.json()["status"] == 200
 
 
-def dump_json(json_obj):
+def print_formatted_json(json_obj):
     print(json.dumps(json_obj, sort_keys=True, indent=4))
 
 
 def get_persons():
     req = requests.get(bot_config.GET_USERS_URL, headers=INTERNAL_HEADERS)
     persons_dict = {}
-    if valid_request(req):
-        for person in req.json()["results"]:
-            uid = person["_id"]
+    if is_valid_response_status(req):
+        for entry in req.json()["results"]:
+            uid = entry["_id"]
             if uid in persons_dict: continue  # name, age, profile_images, instagram_images
-            name = person["name"]
+            name = entry["name"]
 
             age = datetime.datetime.now().year - int(
-                person["birth_date"].split('-')[0]) - 1 if "birth_date" in person and len(
-                person["birth_date"]) > 4 else 99
-            distance = int(person["distance_mi"]) if "distance_mi" in person and person["distance_mi"] >= 0 else -1
-            bio = person["bio"] if "bio" in person else ""
+                entry["birth_date"].split('-')[0]) - 1 if "birth_date" in entry and len(
+                entry["birth_date"]) > 4 else 99
+            distance = int(entry["distance_mi"]) if "distance_mi" in entry and entry["distance_mi"] >= 0 else -1
+            bio = entry["bio"] if "bio" in entry else ""
 
             profile_images = []
-            for photo in person["photos"]:
+            for photo in entry["photos"]:
                 profile_images.append(photo["url"])
 
             instagram_images = []
             try:
-                if "instagram" in person and "photos" in person["instagram"]:
-                    for photo in person["instagram"]["photos"]:
+                if "instagram" in entry and "photos" in entry["instagram"]:
+                    for photo in entry["instagram"]["photos"]:
                         if "image" in photo:  # sometimes the nodes dont contain pictures for some reason
                             instagram_images.append(photo["image"])
             except Exception as e:
-                print("[ERROR] Unable to fetch Instagram data from profile. Dumping JSON data. |=> {}".format(e))
-                dump_json(person)
+                print(f"[ERROR] Unable to fetch Instagram data from profile. Dumping JSON data. |=> {e}")
+                print_formatted_json(entry)
 
-            persons_dict[uid] = Person(name, age, distance, bio, profile_images, instagram_images)
+            persons_dict[uid] = Profile(name, age, distance, bio, profile_images, instagram_images)
     else:
-        print("[ERROR] Invalid response code when fetching new batch of persons.")
+        print("[ERROR] Invalid response code when fetching new batch of profiles. "
+              "Consider generating a new auth token if this error persists.")
     return persons_dict
 
 
 def swipe(uid, right):
     try:
-        req = requests.get('{}/{}'.format(bot_config.LIKE_USER_URL if right else bot_config.DISLIKE_USER_URL, uid),
-                           headers=INTERNAL_HEADERS)
-        if req.json()["status"] != 200:
-            print("[ERROR]\t\tSwipe rejected, consider getting a new key if this happens again.")
+        url = bot_config.LIKE_USER_URL if right else bot_config.DISLIKE_USER_URL
+        req = requests.get('{}/{}'.format(url, uid), headers=INTERNAL_HEADERS)
+        if not is_valid_response_status(req):
+            print("[ERROR]\t\tSwipe rejected, consider generating a new auth token if this happens again.")
     except Exception as e:
-        print("[ERROR]\t\tSwipe failed due to unknown error. |=> {}".format(e))
+        print(f"[ERROR]\t\tSwipe failed due to unhandled error. |=> {e}")
 
 
 def write_image_file(instagram, url, age, uid, index, total):
@@ -139,18 +141,19 @@ def write_image_file(instagram, url, age, uid, index, total):
     if r.status_code == 200:  # success
         r.raw.decode_content = True
         file_extension = url.split('?')[0].split('.')[-1] if instagram else url.split('.')[-1]
-        path = 'output/image_output/{}'.format(age)
+        path = f'{bot_config.IMAGE_OUTPUT_PATH}/{age}'
         if not os.path.exists(path):
             try:
                 os.makedirs(path)
             except Exception as e:
-                print("[WARN] Thread collision when checking if path exists. |=> {}".format(e))  # not a big deal, happens occasionally
+                print(
+                    f"[WARN] Thread collision when checking if path exists. |=> {e}")  # not a big deal, happens occasionally
         try:
             filename = "{}/{}_{}_{}.{}".format(path, uid, "IG" if instagram else "PP", index, file_extension)
             with open(filename, 'wb') as f:
                 shutil.copyfileobj(r.raw, f)
         except Exception as e:
-            print("[WARN] Unable to write file to disk, skipping file. |=> {}".format(e))
+            print(f"[WARN] Unable to write file to disk, skipping file. |=> {e}")
     else:
         print("[WARN]\t\tFailed to download {} picture {}/{} from uid {}.".format(
             "Instagram" if instagram else "profile",
@@ -158,73 +161,102 @@ def write_image_file(instagram, url, age, uid, index, total):
 
 
 def start_bot(token_input):
-    print("[INFO] Starting bot..")
+    print(f"[INFO] Starting bot with {bot_config.POOL_WORKERS} workers..")
     INTERNAL_HEADERS['x-auth-token'] = token_input
     now = datetime.datetime.now()
-    ostream = open("output/data_{}-{}-{}__{}-{}.txt".format(now.day, now.month, now.year, now.minute, now.second), 'a+',
-                   encoding='utf-16')
+    filename = f"{bot_config.DATA_OUTPUT_PATH}/data_{now.day}-{now.month}-{now.year}__{now.hour}-{now.minute}-{now.second}.txt"
+    ostream = open(filename, 'a+',
+                   encoding=bot_config.DATA_OUTPUT_ENCODING)
+    ostream.write(''.format())  # TODO print bot config settings
     swipes = 0
-    with Pool(bot_config.POOL_WORKERS) as pool:
+    fails = 0
+    with Pool(min(bot_config.POOL_WORKERS, bot_config.MAX_POSSIBLE_PROFILE_IMAGES)) as pool:
         while True:
             persons = get_persons()
             if len(persons) <= 0:
-                print("[INFO] No more matches returned, ending script.")
-                break
-
-            print("[INFO] =====> Fetched new batch of {} profiles.".format(len(persons)))
-
-            for uid, person in persons.items():
-                time.sleep(bot_config.DELAY_INTERVAL_SECONDS)
-
-                right = bot_config.SWIPE_MAX_AGE >= person.age >= bot_config.SWIPE_MIN_AGE and bot_config.SWIPE_MAX_DISTANCE >= person.distance >= bot_config.SWIPE_MIN_DISTANCE and len(
-                    person.profile_images) >= bot_config.SWIPE_MIN_IMAGES
-
-                swipes += 1
-                print("[INFO] ({}) Swiping {} on {}".format(swipes, 'right' if right else 'left', uid))
-
-                swipe(uid, right)
-
-                if person.age <= bot_config.IMAGE_AGE_MAX:
-                    print("[INFO]\t\tDownloading {} profile pictures and {} Instagram pictures.".format(
-                        len(person.profile_images), len(person.instagram_images)))
-                    try:
-                        workers = []
-                        if bot_config.IMAGE_PROFILE:
-                            index = 0
-                            for url in person.profile_images:
-                                workers.append(pool.apply_async(write_image_file, (
-                                    False, url, person.age, uid, index, len(person.profile_images),)))
-                                index += 1
-
-                        if bot_config.IMAGE_INSTAGRAM:
-                            index = 0
-                            for url in person.instagram_images:
-                                workers.append(pool.apply_async(write_image_file, (
-                                    True, url, person.age, uid, index, len(person.instagram_images),)))
-                                index += 1
-
-                        if len(workers) > 0:
-                            [res.get(bot_config.WORKER_TIMEOUT_SECONDS) for res in workers]  # doesnt return anything
-                    except Exception as e:
-                        print("[WARN] Timeout occurred when trying to download an image. |=> {}".format(e))
+                if fails < bot_config.EMPTY_RESPONSE_ATTEMPTS:
+                    print("[INFO] No matches returned, trying again in two seconds...")
+                    fails += 1
+                    time.sleep(2)  # seconds
+                    continue
                 else:
-                    print("[INFO]\t\tSkipping pictures on person age {}".format(person.age))
+                    print("[ERROR] No matches returned, ending script. Consider generating a new token or changing "
+                          "swipe location.")
+                    break
+            else:
+                fails = 0
+
+            print(f"[INFO] =====> Fetched new batch of {len(persons)} profiles.")
+
+            for uid, profile in persons.items():
+                time.sleep(bot_config.API_REQUEST_DELAY_SECONDS)
+
+                if bot_config.AUTO_SWIPE:
+                    right = (bot_config.SWIPE_MAX_AGE >= profile.age >= bot_config.SWIPE_MIN_AGE
+                             and bot_config.SWIPE_MAX_DISTANCE >= profile.distance >= bot_config.SWIPE_MIN_DISTANCE
+                             and len(profile.profile_images) >= bot_config.SWIPE_MIN_IMAGES)
+                    swipes += 1
+                    print("[INFO] ({}) Swiping {} on {}".format(swipes, 'right' if right else 'left', uid))
+                    swipe(uid, right)
+
+                if bot_config.DOWNLOAD_IMAGE_PROFILE and bot_config.DOWNLOAD_IMAGE_INSTAGRAM:
+                    if profile.age <= bot_config.DOWNLOAD_IMAGE_AGE_MAX:
+                        # TODO make language reflect which ones are really being downloaded (based on config)
+                        print(f"[INFO]\t\tDownloading {len(profile.profile_images)} profile "
+                              f"pictures and {len(profile.instagram_images)} Instagram pictures.")
+                        try:
+                            workers = []
+                            if bot_config.DOWNLOAD_IMAGE_PROFILE:
+                                index = 0
+                                for url in profile.profile_images:
+                                    workers.append(pool.apply_async(write_image_file, (
+                                        False, url, profile.age, uid, index, len(profile.profile_images),)))
+                                    index += 1
+
+                            if bot_config.DOWNLOAD_IMAGE_INSTAGRAM:
+                                index = 0
+                                for url in profile.instagram_images:
+                                    workers.append(pool.apply_async(write_image_file, (
+                                        True, url, profile.age, uid, index, len(profile.instagram_images),)))
+                                    index += 1
+
+                            [res.get(bot_config.WORKER_TIMEOUT_SECONDS) for res in workers]
+                        except Exception as e:
+                            print(f"[WARN] Timeout occurred when trying to download an image. |=> {e}")
+                    else:
+                        print(f"[INFO]\t\tSkipping pictures on profile with age {profile.age}")
 
                 if bot_config.LOG_PROFILE_DATA:
-                    ostream.write(
-                        '{};{};{};{};{};{};{};{}\n'.format(uid, person.name, person.age, person.distance, right,
-                                                           person.profile_images, person.instagram_images,
-                                                           repr(person.bio)))
+                    print(f"[INFO] Writing log data..")
+                    sepr = bot_config.PROFILE_DATA_SEPARATOR
+                    log_string = f"{uid}{sepr}"
+                    if bot_config.LOG_PROFILE_NAME:
+                        log_string += f"{profile.name}{sepr}"
+                    if bot_config.LOG_PROFILE_AGE:
+                        log_string += f"{profile.age}{sepr}"
+                    if bot_config.LOG_PROFILE_DISTANCE:
+                        log_string += f"{profile.distance}{sepr}"
+                    if bot_config.LOG_PROFILE_SWIPE and bot_config.AUTO_SWIPE:
+                        log_string += f"{right}{sepr}"
+                    if bot_config.LOG_PROFILE_BIO:
+                        log_string += f"{repr(profile.bio.replace(sepr, ''))}{sepr}"
+                    if bot_config.LOG_PROFILE_IMAGE_URLS:
+                        log_string += f"{profile.profile_images}{sepr}"
+                    if bot_config.LOG_PROFILE_INSTAGRAM_URLS:
+                        log_string += f"{profile.instagram_images}{sepr}"
 
+                    ostream.write(f"{log_string}\n")
 
-INTERNAL_HEADERS = bot_config.HEADERS.copy()  # these headers are used for non-auth actions
 
 if __name__ == '__main__':  # required because we spin up a bunch of workers
+    # these headers are used for non-auth request actions (swiping, requesting new profiles, etc)
+    INTERNAL_HEADERS = bot_config.HEADERS.copy()
     token = get_token_if_exist()
     if len(token) < bot_config.MIN_TOKEN_LENGTH:
         token = generate_new_token()
     else:
-        print("[INFO] Existing auth key detected. Delete auth file if access errors occur frequently.")
+        print("[INFO] Existing auth key detected. Delete and regenerate "
+              "auth file if access-related errors occur frequently.")
 
     if token is not '':
         start_bot(token)
